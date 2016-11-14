02772ca8a591ceb4e6ea4f85dbee9408e2768d95
helenawenzin <helma254@hotmail.com>
2016-11-14 19:42:45 +0100

    after and before functions added

--- src/test/java/se/bettercode/seleniumstarter/MyFirstTest.java
+++ src/test/java/se/bettercode/seleniumstarter/MyFirstTest.java
@@ -1,43 +1,55 @@
 package se.bettercode.seleniumstarter;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.junit.Assert.assertEquals;
 
 public class MyFirstTest {
 
+    WebDriver driver;
+
+    @Before
+    public void startBrowserAndGoToNok() {
+
+        driver = new HtmlUnitDriver();
+        driver.get("http://www.nok.se");
+    }
+
+    @After
+    public void closeBrowser() {
+        driver.close();
+    }
+
     @Test
     public void trySomething(){
 
-        WebDriver driver = new HtmlUnitDriver();
-
-        driver.get("http://www.nok.se");
-
+        startBrowserAndGoToNok();
         assertEquals("Natur & Kultur - Natur och Kultur", driver.getTitle());
     }
 
     @Test
     public void checkLoginButton() {
 
-        WebDriver driver = new HtmlUnitDriver();
-
-        driver.get("http://www.nok.se");
+        startBrowserAndGoToNok();
 
         driver.findElement(By.id("ctl00_cphTotalRegion_cphWrapperRegion_cphContentRegion_uContentArea_ctl00_lbLogin"));
 
     }
 
     @Test
-    public void login() {
+    public void login() throws InterruptedException {
 
-        WebDriver driver = new HtmlUnitDriver();
-
-        driver.get("http://www.nok.se");
+        startBrowserAndGoToNok();
 
         WebElement id = driver.findElement(By.name("ctl00$cphTotalRegion$cphWrapperRegion$cphContentRegion$uContentArea$ctl00$txtCheckoutUserName"));
         WebElement pass = driver.findElement(By.name("ctl00$cphTotalRegion$cphWrapperRegion$cphContentRegion$uContentArea$ctl00$txtCheckoutPassword"));
