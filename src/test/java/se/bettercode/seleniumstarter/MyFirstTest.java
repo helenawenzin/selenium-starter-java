7a309db1ca1db3cf15b18a187104209308c6d1e3
helenawenzin <helma254@hotmail.com>
2016-11-14 16:06:12 +0100

    login test

--- src/test/java/se/bettercode/seleniumstarter/MyFirstTest.java
+++ src/test/java/se/bettercode/seleniumstarter/MyFirstTest.java
@@ -3,6 +3,8 @@ package se.bettercode.seleniumstarter;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import static org.junit.Assert.assertEquals;
@@ -30,6 +32,20 @@ public class MyFirstTest {
 
     }
 
+    @Test
+    public void login() {
 
+        WebDriver driver = new HtmlUnitDriver();
+
+        driver.get("http://www.nok.se");
+
+        WebElement id = driver.findElement(By.name("ctl00$cphTotalRegion$cphWrapperRegion$cphContentRegion$uContentArea$ctl00$txtCheckoutUserName"));
+        WebElement pass = driver.findElement(By.name("ctl00$cphTotalRegion$cphWrapperRegion$cphContentRegion$uContentArea$ctl00$txtCheckoutPassword"));
+        WebElement loginButton =  driver.findElement(By.id("ctl00_cphTotalRegion_cphWrapperRegion_cphContentRegion_uContentArea_ctl00_lbLogin"));
+
+        id.sendKeys("Test.Larare@nok.se");
+        pass.sendKeys("testLarare");
+        loginButton.click();
+    }
 
 }
