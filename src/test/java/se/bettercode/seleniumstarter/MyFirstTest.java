5fc93e69b029514c3969b6a396eda69d761ecad0
helenawenzin <helma254@hotmail.com>
2016-11-24 14:01:23 +0100

    clean up after ws day...

--- src/test/java/se/bettercode/seleniumstarter/MyFirstTest.java
+++ src/test/java/se/bettercode/seleniumstarter/MyFirstTest.java
@@ -3,15 +3,7 @@ package se.bettercode.seleniumstarter;
 import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-
-import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 
@@ -34,17 +26,12 @@ public class MyFirstTest {
 
     @Before
     public void before(){
-        driver.get("http://www.nok.se");
+        driver.get("http://www.nok.se/loggain");
     }
 
     @Test
     public void trySomething(){
-        assertEquals("Natur & Kultur - Natur och Kultur", driver.getTitle());
-    }
-
-    @Test
-    public void checkLoginButton() {
-        driver.findElement(By.id("ctl00_cphTotalRegion_cphWrapperRegion_cphContentRegion_uContentArea_ctl00_lbLogin"));
+        assertEquals("NoK", driver.getTitle());
     }
 
     @Test
@@ -58,10 +45,11 @@ public class MyFirstTest {
     }
 
     @Test
+    @Ignore
     public void search() {
 
         nokController.getSearchElement().sendKeys("rivstart");
-        
+
     }
 
 
